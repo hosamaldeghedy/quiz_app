716fa9f40a96cab99b7e8249d319095e1784caad
unknown <h.aldeghedy@gmail.com>
2025-12-23 01:12:56 +0200

    my quiz app

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gen/assets.gen.dart';
+import 'quiz_app.dart';
+void main() {
+  runApp( QuizApp());
+  Assets.images.galaxy.image();
+}
